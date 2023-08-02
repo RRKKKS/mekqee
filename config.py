e43a772f72df61679d911988950402ec6822d77d
venom01k <135177210+venom01k@users.noreply.github.com>
2023-08-03 00:32:41 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+from sample_config import Config
+class Development(Config):
+
+    APP_ID = "26022994"
+    
+    API_HASH = "2e84a6b68bd6b5a79f46e8192668e0ea"
+
+    ALIVE_NAME = "dady"
+
+    DB_URI = "postgresql://postgres:pass@localhost:5432/kk"
+
+    STRING_SESSION = "BAA3ho7zZ8gRibgOlatZ4wLCXGA02fFduAq-IURbQR0tj-K-uoIfqXMLq3HDXCwp7xcNdFvWluhgDDlEr0uCfFoWUuy5V_nzG6AzMUhkpk5XeuhJaH2Nh_9IOq_S_T2iTunCH_7U6RusruA-QwecE_ocJ4tK5EmImSWNpfMpVIAY8IZy1vih1quGou53ETgh2zE3K1qp7W32pRrCrtPLqsEg1T63p4mEb_ZAkHtUA3aCLNm7MUGwnCbkdnpQ1Qu8nPb_4sJBlZnDtBUrjr6SyGG_8OeBTvFvO7DllyNJvzKmqvx7pvbqAvRVVb5331cvFsIzJFgcbbVhNj9s6oN4e66eAAAAASpcpqYA"
+
+    TG_BOT_TOKEN = "6267918080:AAEDvYQ-xILqWEVM4shVORogbUB7BV35xM0"
+
+    PRIVATE_GROUP_BOT_API_ID = "-100928437206"
+
+    COMMAND_HAND_LER = "."
+
+    SUDO_COMMAND_HAND_LER = "."
